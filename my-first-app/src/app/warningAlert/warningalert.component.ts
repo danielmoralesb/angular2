90306f0233ac94c38c3ce82c5391d8fc91118864
Morales <796141@cognizant.com>
2023-12-06 13:38:06 -0500

    component exercises

---
+++ my-first-app/src/app/warningAlert/warningalert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warningalert.component.html',
+  styleUrl: './warningalert.component.scss',
+})
+export class WarningAlertComponent {}
